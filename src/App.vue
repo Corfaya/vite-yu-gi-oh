27537dda161c0f4653356b23868d395b61fabd45
Corfaya <lauracorrado0696@gmail.com>
2024-07-31 14:54:01 +0200

    [UPD] Main components created

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <script>
 import axios from 'axios';
 import { store } from './store';
+
+import Header from './components/Header.vue';
+import Main from './components/Main.vue';
+
 export default {
+  components: {
+    Header,
+    Main
+  },
   data() {
     return {
       store
@@ -21,7 +29,8 @@ export default {
 </script>
 
 <template>
-
+  <Header />
+  <Main />
 </template>
 
 <style scoped>
