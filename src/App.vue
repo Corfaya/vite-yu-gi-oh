657900466ad16ff816397e049247264201dc6402
Corfaya <lauracorrado0696@gmail.com>
2024-08-01 17:05:41 +0200

    test

--- src/App.vue
+++ src/App.vue
@@ -17,22 +17,19 @@ export default {
   },
   created() {
     this.getCards(),
-      this.getArchetype()
-  },
-  computed: {
-
+    this.getArchetype()
   },
   methods: {
     getCards() {
+      let url = `${store.url}`
+      if (store.archetypeCard != "") {
+        url += `?archetype_name=${store.archetypeList}`
+      }
       axios.get(`${store.url}${store.apiCards}`).then((res) => {
         store.cardList = res.data.data
       })
     },
     getArchetype() {
-      let url = `${store.url}`
-      if (store.archetypeCard != "") {
-        url += `?archetype_name=${store.archetypeList}`
-      }
       axios.get(`${store.url}${store.apiArch}`).then((res) => {
         //console.log(res)
         for (let i = 0; i < 10; i++) {
--- src/store.js
+++ src/store.js
@@ -2,8 +2,9 @@ import { reactive } from "vue"
 export const store = reactive ({
     cardList: [],
     archetypeList: [],
+    archetypeCard: "",
+    
     url: "https://db.ygoprodeck.com/api/v7/",
     apiCards: "cardinfo.php?num=50&offset=30",
-    apiArch: "archetypes.php",
-    archetypeCard: ""
+    apiArch: "archetypes.php"
 })
